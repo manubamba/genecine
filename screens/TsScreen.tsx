b3889a939b86b54e295f6ff764cd546fa9911d93
Rishabh Jain <risjai@microsoft.com>
2019-03-25 19:55:20 +0530

    Add algolia search and searchbox UI

--- screens/TsScreen.tsx
+++ screens/TsScreen.tsx
@@ -1,13 +1,35 @@
 import * as React from "react";
-import { Text, View } from "react-native";
-import { SearchBar } from "react-native-elements";
+import { Text, View, StyleSheet  } from "react-native";
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch
+} from 'react-instantsearch-native';
+import SearchBox from './SearchBox';
+import InfiniteHits from './InfiniteHits';
+
+const searchClient = algoliasearch(
+  '5QD8782XZG',
+  'fc49e07f378c19fb4f64281a44c76bf3'
+);
+
+const styles = StyleSheet.create({
+  closeButton: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  closeButtonText: {
+    fontSize: 18,
+  },
+});
 
 export const TsScreen = () => (
   <View>
-      <SearchBar
-        placeholder="Type Here..."
-      />
-    <Text>This should work</Text>
+    <InstantSearch indexName="dev_brandedMeds" searchClient={searchClient}>
+    <SearchBox text= "Type the medicine name"/>
+    <InfiniteHits />
+    </InstantSearch>
+    <Text>{"\n"} That's all for now</Text>
   </View>
 );
+
 export default TsScreen;
