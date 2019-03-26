85d86d593d28f52b02b3d9bc99469d3d1038f8c0
Manu Bamba <mabamba@microsoft.com>
2019-03-26 14:41:05 +0530

    WIP

---
+++ screens/InfiniteHits.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Highlight from './Highlight';
+import { connectInfiniteHits } from 'react-instantsearch-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  separator: {
+    borderBottomWidth: 1
+  },
+  item: {
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+});
+
+interface InfiniteHitsProps {
+  hits: Item[];
+  hasMore: boolean;
+  refine: () => {};
+}
+
+interface Item {
+  objectID: string;
+}
+
+const InfiniteHits = ({ hits, hasMore, refine }: InfiniteHitsProps) => (
+  <FlatList
+    data={hits}
+    keyExtractor={(item: Item) => item.objectID}
+    ItemSeparatorComponent={() => <View style={styles.separator} />}
+    onEndReached={() => hasMore && refine()}
+    renderItem={({ item }) => (
+      <View style={styles.item}>
+        <Highlight attribute="name" hit={item} />
+      </View>
+    )}
+  />
+);
+
+export default connectInfiniteHits(InfiniteHits);
--- storybook/index.ts
+++ storybook/index.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import { configure, getStorybookUI } from '@storybook/react-native';
 import { loadStories } from './storyLoader';
@@ -8,10 +9,29 @@ configure(loadStories, module);
 
 // Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+// const StorybookUIRoot = getStorybookUI({});
 
 // If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
 // If you use Expo you can safely remove this line.
-AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
+// AppRegistry.registerComponent('%APP_NAME%', () => StorybookUIRoot);
 
-export default StorybookUIRoot;
+// export default StorybookUIRoot;
+
+// Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({ port: 7007, onDeviceUI: true });
+
+// react-native hot module loader must take in a Class - https://github.com/facebook/react-native/issues/10991
+// https://github.com/storybooks/storybook/issues/2081
+// eslint-disable-next-line react/prefer-stateless-function
+class StorybookUIHMRRoot extends React.Component {
+  render() {
+    return <StorybookUIRoot />;
+  }
+}
+
+// If you are using React Native vanilla and after installation you don't see your app name here, write it manually.
+// If you use Expo you can safely remove this line.
+// AppRegistry.registerComponent("ExportPro", () => StorybookUIRoot);
+
+export default StorybookUIHMRRoot;
