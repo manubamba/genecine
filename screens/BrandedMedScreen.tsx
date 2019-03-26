e9e7936ae665166e91d4a8b37a1a775d34dc1583
Manu Bamba <mabamba@microsoft.com>
2019-03-26 16:16:40 +0530

    WIP

--- screens/BrandedMedScreen.tsx
+++ screens/BrandedMedScreen.tsx
@@ -1,68 +1,181 @@
 import * as React from 'react';
+import gql from 'graphql-tag';
+import { NavigationScreenProp } from 'react-navigation';
+import { Query } from 'react-apollo';
+import {
+    ScrollView,
+    StyleProp,
+    TextStyle,
+    TouchableOpacity,
+    View
+    } from 'react-native';
 import { Text } from 'react-native-elements';
-import { View } from 'react-native';
 
 interface Ingredient {
   name: string;
   potency: string;
 }
 
-interface BrandedAlternative {
+interface MedPriceDetail {
   name: string;
   price: string;
-  pharmacy: string;
+  pharmacy?: string;
 }
 
 interface BrandedMedScreenProps {
   ingredients: Ingredient[];
-  genericMedName: string;
-  brandedAlternatives?: BrandedAlternative[];
+  genericMed: MedPriceDetail;
+  brandedAlternatives?: MedPriceDetail[];
   uses?: string[];
+  navigation?: NavigationScreenProp<any, any>;
 }
 
 export const TextLabel = ({ children }: { children: string }) => (
-  <Text h4 h4Style={{ fontWeight: 'bold' }}>
-    {children}:{' '}
-  </Text>
-);
-
-export const TextValue = ({ children }: { children: string }) => (
   <Text
     h4
     h4Style={{
-      fontWeight: '100',
-      fontStyle: 'italic'
+      fontWeight: 'bold',
+      textTransform: 'uppercase',
+      color: '#234d20',
+      textAlign: 'center',
+      marginBottom: 5
     }}
   >
     {children}
   </Text>
 );
 
-export default ({
+export const Space = () => (
+  <View
+    style={{
+      height: 25
+    }}
+  />
+);
+
+export const TextValue = ({
+  children,
+  style = {}
+}: {
+  children: string;
+  style?: StyleProp<TextStyle>;
+}) => (
+  <Text
+    style={[
+      {
+        fontWeight: '100',
+        fontStyle: 'italic',
+        marginLeft: 10,
+        fontSize: 18
+      },
+      style
+    ]}
+  >
+    {children}
+  </Text>
+);
+
+export const PriceValue = ({ children }: { children: string }) => (
+  <TextValue
+    style={{
+      color: '#007f0e'
+    }}
+  >
+    {children}
+  </TextValue>
+);
+
+export const NameAndPrice = ({
+  name,
+  price
+}: {
+  name: string;
+  price: string;
+}) => (
+  <Text>
+    <TextValue>{`${name} - `}</TextValue>
+    <PriceValue>{`₹${price}`}</PriceValue>
+  </Text>
+);
+
+const navigateToMedPage = () => {};
+
+const GET_MED = (id: string) => gql`{
+    medicine(${id}) {
+        ingredients
+        genericMed
+        brandedAlternatives
+        uses
+    }
+}`;
+
+export const BrandedMedScreen = ({
   ingredients,
-  genericMedName,
-  brandedAlternatives
+  genericMed,
+  brandedAlternatives,
+  uses,
+  navigation
 }: BrandedMedScreenProps) => (
-  <View>
+  <ScrollView
+    style={{
+      marginHorizontal: 10,
+      height: '100%'
+    }}
+  >
     <TextLabel>Active ingredients</TextLabel>
     {ingredients.map(ingredient => (
       <TextValue key={ingredient.name}>{`${ingredient.name}(${
         ingredient.potency
       })`}</TextValue>
     ))}
-
+    <Space />
     <TextLabel>Generic Name</TextLabel>
-    <TextValue>{genericMedName}</TextValue>
-
+    <NameAndPrice {...genericMed} />
+    <Space />
     {brandedAlternatives && brandedAlternatives.length && (
       <View>
-        {brandedAlternatives.map(brandedAlternative => [
-          <TextValue key={brandedAlternative.name}>{`${
-            brandedAlternative.name
-          } - ₹${brandedAlternative.price}`}</TextValue>,
-          <Text>{brandedAlternative.pharmacy}</Text>
-        ])}
+        <TextLabel>Branded alternatives</TextLabel>
+        {brandedAlternatives.map(brandedAlternative => (
+          <TouchableOpacity
+            key={brandedAlternative.name}
+            onPress={() => navigateToMedPage()}
+          >
+            <NameAndPrice {...brandedAlternative} />
+            <Text
+              style={{
+                marginBottom: 5
+              }}
+            >
+              {brandedAlternative.pharmacy}
+            </Text>
+          </TouchableOpacity>
+        ))}
+        <Space />
       </View>
     )}
-  </View>
+    {uses && uses.length && (
+      <View>
+        <TextLabel>Uses</TextLabel>
+        {uses.map(use => (
+          <TextValue key={use}>{use}</TextValue>
+        ))}
+      </View>
+    )}
+  </ScrollView>
+);
+
+export default ({ navigation }: BrandedMedScreenProps) => (
+  <Query query={GET_MED(navigation && navigation.getParam('id'))}>
+    {({ loading, error, data }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+
+      return (
+        <BrandedMedScreen
+          genericMed={data.genericMed}
+          ingredients={data.ingredients}
+        />
+      );
+    }}
+  </Query>
 );
